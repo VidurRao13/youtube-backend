2e3d7a5313f6ab09ed66818b6b0461c36f86f6fc
Vidur Rao <vidurrao4@gmail.com>
2025-08-20 18:58:50 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,3 +8,11 @@ const asyncHandler = (fn) => async (req, res, next) => {
         })
     }
 }
+
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+//     }
+// }
+
+export {asyncHandler}
