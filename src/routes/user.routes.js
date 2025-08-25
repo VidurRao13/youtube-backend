682452a7ba12144841e55625c3244b7875923fa8
Vidur Rao <vidurrao4@gmail.com>
2025-08-25 13:27:51 +0530

    adding login logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controllers.js";
+import {loginUser, registerUser, logoutUser} from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.js";
+import { verifyJWT } from "../middlewares/auth.js";
 
 const router = Router()
 
@@ -17,5 +18,10 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+// secured router
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
